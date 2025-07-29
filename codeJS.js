bf71444fb94e94eae6de551a1150ba6c17c63c7d
Naythan <156268447+goughn@users.noreply.github.com>
2025-07-29 06:56:48 -0400

    FOR CDN!!!!!!!

--- codeJS.js
+++ codeJS.js
@@ -1,5 +1,5 @@
 var pyodideReadyPromise = loadPyodide();
-console.log("type 106 github vB8.2");
+console.log("type 106 github vB8.3");
 console.log("=== codeJS.js LOADED ===", new Date().toISOString());
 function createTextArea() {
     // Find the first element with class 'instanceHolder'
